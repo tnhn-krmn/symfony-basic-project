df7cfe75b6e62e1f3c745d9a2e2157d56ecfc694
Tunahan Karaman <krmntunahan@gmail.com>
2022-04-26 12:18:31 +0300

    kullanıcı giriş ve kayıt sayfaları oluşturuldu

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        return $this->render('admin/dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
