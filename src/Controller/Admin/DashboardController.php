40383b66d8de23f2193707c95fe361b1aa844b87
Tunahan Karaman <krmntunahan@gmail.com>
2022-04-26 20:43:41 +0300

    kategori ekleme işlemi yapıldı

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,17 +3,60 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
 
 class DashboardController extends AbstractController
 {
+
+
+    protected $doctrine;
+
+    public function __construct(PersistenceManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+        ]);
+    }
+
+    #[Route('/admin/ticket-new', name: 'ticket_new')]
+    public function categoryAdd(Request $request): Response
+    {
+        $category = new Category();
+
+        $form = $this->createFormBuilder($category)
+            ->add('name', TextType::class)
+            ->add('Save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->doctrine->getManager();
+            $category = $form->getData();
+            $em->persist($category);
+            $em->flush();
+
+
+            return  $this->redirect('/admin/dashboard');
+        }
+
+        return $this->render('admin/dashboard/category-add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
